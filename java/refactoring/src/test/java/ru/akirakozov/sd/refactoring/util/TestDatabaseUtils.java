018b73303825567646cfb4a59cb2202df82c0fd2
Arsen Nagdalian <nagoarsen@mail.ru>
2020-10-25 20:21:40 +0300

    Simplify database utility class for tests

--- java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/util/TestDatabaseUtils.java
+++ java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/util/TestDatabaseUtils.java
@@ -1,31 +1,20 @@
 package ru.akirakozov.sd.refactoring.util;
 
+import ru.akirakozov.sd.refactoring.database.DatabaseUtils;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestDatabaseUtils {
     public static void clearDatabase() {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "drop table if exists product";
-            c.prepareStatement(sql).execute();
-        } catch (SQLException e) {
-            System.err.printf("Failed to clear table \"products\": %s\n", e.getMessage());
-            throw new RuntimeException(e);
-        }
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = """
+        DatabaseUtils.executeQuery("drop table if exists product");
+        DatabaseUtils.executeQuery("""
                     create table if not exists product(
                         id integer primary key autoincrement not null,
                         name text not null,
                         price int not null
                     )
-                    """;
-            c.prepareStatement(sql).execute();
-        } catch (SQLException e) {
-            System.err.printf("Failed to clear table \"products\": %s\n", e.getMessage());
-            throw new RuntimeException(e);
-        }
+                    """);
     }
 }
