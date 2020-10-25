f66ea565aa9e40b0ca42a25451c24d65ecab9375
Arsen Nagdalian <nagoarsen@mail.ru>
2020-10-25 20:06:42 +0300

    Replace raw values in Main with final fields

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/Main.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/Main.java
@@ -8,26 +8,28 @@ import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
 import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
 import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
 /**
  * @author akirakozov
  */
 public class Main {
+    public static final String ROOT = "/";
+    private static final String ADD_PRODUCT = "/add-product";
+    private static final String GET_PRODUCTS = "/get-products";
+    private static final String QUERY = "/query";
+    public static final int PORT = 8081;
+
     public static void main(String[] args) throws Exception {
         DatabaseUtils.createProductsTable();
 
-        Server server = new Server(8081);
+        Server server = new Server(PORT);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
+        context.setContextPath(ROOT);
         server.setHandler(context);
 
-        context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
-        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
-        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
+        context.addServlet(new ServletHolder(new AddProductServlet()), ADD_PRODUCT);
+        context.addServlet(new ServletHolder(new GetProductsServlet()), GET_PRODUCTS);
+        context.addServlet(new ServletHolder(new QueryServlet()), QUERY);
 
         server.start();
         server.join();
